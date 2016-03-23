d9be5478f18356aec060bcc9da9dd405cc66bee2
LuisaMayLuna <lluna16@student.rcc.edu>
2016-03-23 11:33:20 -0700

    update

--- Book/Book1/code.js
+++ Book/Book1/code.js
@@ -1,14 +1,23 @@
        var word = "bottles";
        var count = 99;
-       while (count > 0)  {
-       		console.log(count + " " + word + " of beer on the wall") ;
-       		console.log(count + " " + word + " of beer,") ;
-       		console.log("Take down one, pass it around,") ;
-       		count = count - 1;
-       		if (count > 0)  {       		       	
-       				console.log(count + " "	+ word + " of beer on the wall.") ;
-       		} else {
-       				console.log("No more " + word + " of beer on the wall.") ;
-       		} 
-       	}
+       while (count > 0)  
+       		{
+       			console.log(count + " " + word + " of beer on the wall") ;
+       			document.write(count + " " + word + " of beer on the wall<br><br>");
+       			console.log(count + " " + word + " of beer,") ;
+       			document.write(count + " " + word + " of beer,<br>");
+       			console.log("Take down one, pass it around,") ;
+       			document.write("Take down one, pass it around,<br>");
+       			count = count - 1;
+       			if (count > 0)  
+       				{       		       	
+       					console.log(count + " "	+ word + " of beer on the wall.") ;
+       				} 
+       			else 
+       				{
+       					console.log("No more " + word + " of beer on the wall.") ;
+       					document.write("No more " + word + " of beer on the wall.<br>");
+       				} 
+       		}
 
+       	
